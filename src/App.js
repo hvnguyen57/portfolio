cba58fc8c4e8f570726ea941ebd42e4fbd9afd8d
Henry Nguyen <henryvnguyen57@gmail.com>
2023-01-18 00:03:09 -0600

    finished footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
+import Footer from './components/Footer/Footer';
 
 const App = () =>{
   return(
@@ -14,6 +15,7 @@ const App = () =>{
       <Home/>
       <About />
       <Skills />
+      <Footer />
     </main>
     </>
   )
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer_container container">
+                <h1 className="footer_title">Nguyen</h1>
+                <ul className="footer_list">
+                    <li>
+                        <a href="#about" className="footer_link">About</a>
+                    </li>
+
+                    <li>
+                        <a href="#skills" className="footer_link">Skills</a>
+                    </li>
+
+                    <li>
+                        <a href="#portfolio" className="footer_link">Portfolio</a>
+                    </li>
+
+                    <li>
+                        <a href="#contact" className="footer_link">Contact</a>
+                    </li>
+                </ul>
+                <div className="footer_social">
+                    <a href="https://www.instagram.com/hhnrye_n/" className="home_social-icon" target="_blank" rel="noreferrer">
+                        <i class="uil uil-instagram"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/hvnguyen/" className="home_social-icon" target="_blank" rel="noreferrer">
+                        <i class="uil uil-linkedin"></i>
+                    </a>
+
+                    <a href="https://github.com/hvnguyen57" className="home_social-icon" target="_blank" rel="noreferrer">
+                        <i class="uil uil-github"></i>
+                    </a>
+                </div>
+
+                <span className="footer_copy">&#169; Henry Nguyen. All rights reserved</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
